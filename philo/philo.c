68c722ab1f535536f0f118b64a811dbe5c36d8da
successfulfadwa <fadwa.aljaoui@gmail.com>
2022-07-16 05:24:16 +0100

    protect args

--- philo/philo.c
+++ philo/philo.c
@@ -6,12 +6,19 @@
 /*   By: fadwa <fadwa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 03:26:30 by fadwa             #+#    #+#             */
-/*   Updated: 2022/07/16 05:05:15 by fadwa            ###   ########.fr       */
+/*   Updated: 2022/07/16 05:23:22 by fadwa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-int main()
+int main(int argc , char *argv[])
 {
-	
+	//t_setup setup;
+	(void)argv;
+	if (argc <5 || argc >6)
+	{
+		printf("Invalid syntax!\n");
+		exit(1);
+	}
+	printf("hey");
 }
