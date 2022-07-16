c427e4cc12f12bf5e8b50063b3271dd288841af5
successfulfadwa <fadwa.aljaoui@gmail.com>
2022-07-16 05:05:29 +0100

    add struct

--- philo/philo.h
+++ philo/philo.h
@@ -12,5 +12,23 @@
 # define MAX_INT 2147483647
 # define MIN_INT -2147483648
 
+typedef struct s_philo
+{
+	int		philo_id;
+	int		n_meals;
+	long 	last_meal;
+	int	 	left_fork;
+	int		right_fork;
+
+}	t_philo;
+
+typedef struct s_setup
+{
+	int n_philos;
+	int time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	times_to_eat;
+}	t_setup;
 
 #endif
